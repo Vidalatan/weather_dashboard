c980aea082388360e5b21fac9a2a20e23b5c8516
Vidalatan <vidalatan@gmail.com>
2022-03-15 00:53:55 -0500

    Updated script.js
    Fixed cards showing next 8 days and reduced down to 5 days. Added units
    of measurement and fixed what unit they are measured in on the request to
    openweathermap.org

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,7 +3,7 @@ const APIKey = "556c7224952ff69a1258be2c9abab777";
 
 // Function returns display required content as an object
 function getCurrentCoordsWeather(coords, event=null, city=null) {
-    let requestUrls = "https://api.openweathermap.org/data/2.5/onecall?lat="+coords.lat+"&lon="+coords.lon+"&exclude=minutely,hourly,alerts&appid="+APIKey;
+    let requestUrls = "https://api.openweathermap.org/data/2.5/onecall?lat="+coords.lat+"&lon="+coords.lon+"&exclude=minutely,hourly,alerts&units=imperial&appid="+APIKey;
     let current_day = moment();
     
     fetch(requestUrls)
@@ -32,22 +32,23 @@ function getCurrentCoordsWeather(coords, event=null, city=null) {
             console.log("Neither an event or city were passed into the getCurrentCoordsWeather function");
         }
 
-        $("#display-temp").text(data.current.temp)
-        $("#display-wind_speed").text(data.current.wind_speed)
-        $("#display-humidity").text(data.current.humidity)
+        $("#display-temp").text(data.current.temp+"℉")
+        $("#display-wind_speed").text(data.current.wind_speed+" MPH")
+        $("#display-humidity").text(data.current.humidity+"%")
         $("#display-uvi").addClass(uviSeverity).text(data.current.uvi)
 
         $("#forecast-list").children().remove()
-        for (index in data.daily) {
+        for (index = 0; index < 5; index++) {
             let indexed_day = data.daily[index];
+            console.log(indexed_day);
             let data_icon = "http://openweathermap.org/img/wn/"+data.daily[index].weather[0].icon+"\@2x.png"
 
             $("#forecast-list").append($("<div>").addClass("card col mx-2").append($("<h5>").text(current_day.add(1, "days").format("M/D/YYYY")))
             .append($("<img>").attr("src", data_icon).attr("alt", "Weather Condition Icon"))
             .append($("<div>").addClass("card-body")
-            .append($("<div>").addClass("row").text("Temp: "+indexed_day.temp.day+"℉"))
-            .append($("<div>").addClass("row").text("Wind: "+indexed_day.wind_speed+" MPH"))
-            .append($("<div>").addClass("row").text("Humidity: "+indexed_day.humidity+"%"))                
+            .append($("<div>").addClass("row my-2").text("Temp: "+indexed_day.temp.day+"℉"))
+            .append($("<div>").addClass("row my-2").text("Wind: "+indexed_day.wind_speed+" MPH"))
+            .append($("<div>").addClass("row my-2").text("Humidity: "+indexed_day.humidity+"%"))                
             ))
         }
     })
